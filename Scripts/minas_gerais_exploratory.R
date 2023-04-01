685fbaacf1ad46510a72f2595497f07abd291849
Jordan Eckert <jordan@eckert.network>
2023-04-01 16:08:23 -0500

    Finished data cleaning

--- Scripts/minas_gerais_exploratory.R
+++ Scripts/minas_gerais_exploratory.R
@@ -1,25 +1,25 @@
 # The purpose of this script is to perform exploratory spatial data analysis (ESDA) on the Minas Gerais database
 ## This work is in conjunction with Nedret Billor and J.J. Lelis
 
-library(mise)
-mise()
-
-# Attach packages
+# Attach packages - Data Cleaning
 library(readxl)       # Loading excel dataset
 library(tidyverse)    # Data manipulation
 library(dplyr)        # Data manipulation
 library(magrittr)     # Pipe operator
 
-
+# Attach packages - Exploratory Data Analysis
 library(umap)         # Uniform manifold approximations and projections for exploring clustering patterns
+library(adegraphics)  # Graphical representation
+library(spdep)        # Spatial dependency
+
+# Attach packages - Spatial Analysis
 library(pcds)         # Proximity catch digraphs for spatial classification
 library(sp)           # Management of spatial data
 library(terra)        # Spatial data analysis
 library(ade4)         # Multivariate analysis
-library(adegraphics)  # Graphical representation
-library(spdep)        # Spatial dependency
 library(adespatial)   # Multivariate spatial analysis
 
+#### Data Cleaning ####
 # Loading main database
 datum <- read_excel("/Users/basecamp/DataspellProjects/Minas_Gerais/Database/Database.xlsx", skip = 1)
 
@@ -31,34 +31,34 @@ PQL <- PQL[-c(26:32),]  # Removing extra information not needed in R
 str(datum)
 str(PQL)
 
-# Viewing databases
-View(datum)
-View(PQL)
-
-# Changing first four variables
+# Changing descriptive variables
 datum$ID = as.numeric(datum$ID)
 datum$Lab = as.factor(datum$Lab)
 datum$Longitude = as.numeric(datum$Longitude)
 datum$Latitude = as.numeric(datum$Latitude)
 
-# Changing character to numerics in PQL
-PQL <- PQL %>% mutate_at(c("UFV1", "UFOP", "CETEC", "UFV_2"), as.numeric)
-
-## NAs introduced where "-" values was...
-
-# Change PQL NA to 0
-PQL[is.na(PQL)] <- 0
+## NAs that were introduced where "-" values was...
 
 ## Known that a lot of the data will have <PQL values when the heavy metal is below limit ...
 ## This value is causing a lot of numeric variables to register as characters ...
+## The goal then is to replace the <PQL values first ....
+## Therefore, we need to do some slight cleaning with the PQL limits to make this easier...
+## NOTE: `UFV_1' was changed to `UFV1' in the Excel document to match easier here...
+
+# Changing character to numerics in PQL
+PQL <- PQL %>% mutate_at(c("UFV1", "UFOP", "CETEC", "UFV_2"), as.numeric)
+
+# Change any introduced PQL NA to 0
+PQL[is.na(PQL)] <- 0
+
 ## In literature, commonly use half of the PQL in the samples which registered heavy metal content below the PQL...
 
-# Changing table to be half values
+# Changing given PQL values to be the half values we will use
 Elemento = as.factor(PQL$Elemento)
 PQL <- cbind(Elemento, PQL[,2:ncol(PQL)]/2)
-View(PQL)
 
-# Replacing <PQL values
+## Now we are ready to replace <PQL values....
+
 # Loop through each relevant column of data frame
 for (col in names(datum[,5:24])) {
   # Check if the column contains a "<PQL" value
@@ -74,7 +74,30 @@ for (col in names(datum[,5:24])) {
   }
 }
 
-# Coerce all mineral vectors to numerics
+# Coerce all newly changed columns to be numerics
 datum[,5:24] <- lapply(datum[,5:24], as.numeric)
+
+## Now that the heavy metal contents part of the database is cleaned ...
+## I can focus on cleaning the soil properties part ...
+
+# Remove column separating heavy metal and soil properties
+datum$...25 <- NULL
+
+## ph KCl is a character variable, because of "-" token....
+
+# Coerce ph KCL to be numeric
+datum$`pH KCl` <- as.numeric(datum$`pH KCl`)
+
+# Final checks for both datasets
+View(datum)
+View(PQL)
+
+str(datum)
+
+## Just in case it's needed for future reference we have:
+## Heavy metal contents are in columns 5:24 ...
+## Soil Properties are in columns 25:29 ....
+
+#### Exploratory Spatial Data Analysis ####
 
 
